93ff4a7274fee2173fbfc6771539f5238c3edbc3
C Kaustubh <80208387+snigenigmatic@users.noreply.github.com>
2024-09-14 19:43:21 +0530

    Update csll-local1.c

--- CSLL/csll-local1.c
+++ CSLL/csll-local1.c
@@ -1,27 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct node
-{
+typedef struct node {
     int info;
     struct node *next;
 } NODE;
 
 NODE *insertFront(NODE *list, int x);
-void insertEnd(NODE *list, int x);
-void deleteFront();
-void deleteEnd();
+NODE *insertEnd(NODE *list, int x);
+void deleteFront(NODE **list);
+void deleteEnd(NODE **list);
 void position_insert(NODE **list, int x, int pos);
 void position_delete(NODE **list, int pos);
 void display(NODE *list);
-NODE *orderedins(NODE *list, int x);
+NODE *ordered_insert(NODE *list, int x);
 
-void main()
-{
-    int choice, x;
+int main() {
+    int choice, x, pos;
     NODE *list = NULL;
-    while (1)
-    {
+    
+    while (1) {
         printf("1. Insert Front\n");
         printf("2. Insert End\n");
         printf("3. Delete Front\n");
@@ -34,272 +32,220 @@ void main()
         printf("Enter your choice: ");
         scanf("%d", &choice);
 
-        switch (choice)
-        {
-        case 1:
-            printf("Enter the information: ");
-            scanf("%d", &x);
-            list = insertFront(list, x);
-            break;
-
-        case 2:
-            printf("Enter the information: ");
-            scanf("%d", &x);
-            insertEnd(list, x);
-            break;
-
-        case 3:
-            deleteFront(&list);
-            break;
-
-        case 4:
-            deleteEnd(&list);
-            break;
-
-        case 5:
-            int pos;
-            printf("Enter the information and position: ");
-            scanf("%d %d", &x, &pos);
-            position_insert(&list, x, pos);
-            break;
-
-        case 6:
-            printf("Enter the position: ");
-            scanf("%d", &pos);
-            position_delete(&list, pos);
-            break;
-
-        case 7:
-            printf(("Enter the information: "));
-            scanf("%d", &x);
-            ordered_insert(&list, x);
-            break;
-
-        case 8:
-            display(list);
-            break;
-
-        case 9:
-            exit(0);
-            break;
-
-        default:
-            printf("Invalid choice\n");
-            break;
+        switch (choice) {
+            case 1:
+                printf("Enter the information: ");
+                scanf("%d", &x);
+                list = insertFront(list, x);
+                break;
+            case 2:
+                printf("Enter the information: ");
+                scanf("%d", &x);
+                list = insertEnd(list, x);
+                break;
+            case 3:
+                deleteFront(&list);
+                break;
+            case 4:
+                deleteEnd(&list);
+                break;
+            case 5:
+                printf("Enter the information and position: ");
+                scanf("%d %d", &x, &pos);
+                position_insert(&list, x, pos);
+                break;
+            case 6:
+                printf("Enter the position: ");
+                scanf("%d", &pos);
+                position_delete(&list, pos);
+                break;
+            case 7:
+                printf("Enter the information: ");
+                scanf("%d", &x);
+                list = ordered_insert(list, x);
+                break;
+            case 8:
+                display(list);
+                break;
+            case 9:
+                exit(0);
+                break;
+            default:
+                printf("Invalid choice\n");
+                break;
         }
     }
+    return 0;
 }
 
-NODE *insertFront(NODE *list, int x)
-{
-    NODE *newnode, *last;
-    newnode = (NODE *)malloc(sizeof(NODE));
+NODE *insertFront(NODE *list, int x) {
+    NODE *newnode = (NODE *)malloc(sizeof(NODE));
     newnode->info = x;
-    newnode->next = NULL;
-    if (list == NULL)
-    {
-        list = newnode;
+    if (list == NULL) {
+        newnode->next = newnode;
+    } else {
+        NODE *temp = list;
+        while (temp->next != list) {
+            temp = temp->next;
+        }
+        temp->next = newnode;
         newnode->next = list;
-        last = newnode;
     }
-    else
-    {
+    list = newnode;
+    return list;
+}
+
+NODE *insertEnd(NODE *list, int x) {
+    NODE *newnode = (NODE *)malloc(sizeof(NODE));
+    newnode->info = x;
+    if (list == NULL) {
+        newnode->next = newnode;
+        return newnode;
+    } else {
+        NODE *temp = list;
+        while (temp->next != list) {
+            temp = temp->next;
+        }
+        temp->next = newnode;
         newnode->next = list;
-        last->next = newnode;
-        list = newnode;
     }
     return list;
 }
 
-void insertEnd(NODE *list, int x)
-{
-    NODE *newnode = (NODE *)malloc(sizeof(NODE));
-    newnode->info = x;
-    newnode->next = NULL;
-    if (list == NULL)
-    {
-        list = newnode;
-    }
-    else
-    {
-        NODE *temp = list;
-        while (temp->next != NULL)
-        {
-            temp = temp->next;
-        }
-        temp->next = newnode;
-    }
-}
-void display(NODE *list)
-{
-    if (list == NULL)
-    {
+void display(NODE *list) {
+    if (list == NULL) {
         printf("List is empty\n");
+        return;
     }
-    else
-    {
-        NODE *temp;
-        temp = list;
-        while (temp != NULL)
-        {
-            printf("%d\n", temp->info);
-            temp = temp->next;
-        }
-    }
+    NODE *temp = list;
+    do {
+        printf("%d\n", temp->info);
+        temp = temp->next;
+    } while (temp != list);
 }
 
-void deleteFront(NODE **list)
-{
-    if (*list == NULL)
-    {
+void deleteFront(NODE **list) {
+    if (*list == NULL) {
         printf("List is empty\n");
+        return;
     }
-    else
-    {
-        NODE *temp = *list;
+    NODE *temp = *list;
+    if (temp->next == *list) {
+        free(temp);
+        *list = NULL;
+    } else {
+        NODE *last = *list;
+        while (last->next != *list) {
+            last = last->next;
+        }
         *list = (*list)->next;
+        last->next = *list;
         free(temp);
     }
 }
 
-void deleteEnd(NODE **list)
-{
-    if (*list == NULL)
-    {
+void deleteEnd(NODE **list) {
+    if (*list == NULL) {
         printf("List is empty\n");
+        return;
     }
-    else
-    {
-        NODE *temp = *list;
-        if ((*list)->next == NULL)
-        {
-            free(temp);
-            *list = NULL;
-            return;
-        }
+    NODE *temp = *list;
+    if (temp->next == *list) {
+        free(temp);
+        *list = NULL;
+    } else {
         NODE *prev = NULL;
-        while (temp->next != NULL)
-        {
+        while (temp->next != *list) {
             prev = temp;
             temp = temp->next;
         }
-        prev->next = NULL;
+        prev->next = *list;
         free(temp);
     }
 }
-void position_insert(NODE **list, int x, int pos)
-{
-    int i = 1;
+
+void position_insert(NODE **list, int x, int pos) {
     NODE *newnode = (NODE *)malloc(sizeof(NODE));
     newnode->info = x;
-    newnode->next = NULL;
-
-    if (*list == NULL)
-    {
-        *list = newnode; // ignore position for first insertion - first node only
-    }
-    else
-    {
-        NODE *temp = *list;
-        if (pos == 1)
-        {
-            newnode->next = *list;
-            *list = newnode;
-        }
-        else
-        {
-            NODE *prev = NULL;
-            while (temp->next != NULL && i < pos)
-            {
-                prev = temp;
+    if (*list == NULL || pos == 1) {
+        if (*list == NULL) {
+            newnode->next = newnode;
+        } else {
+            NODE *temp = *list;
+            while (temp->next != *list) {
                 temp = temp->next;
-                i++;
             }
-            if (i == pos)
-            {
-                prev->next = newnode;
-                newnode->next = temp;
-            }
-            else if (pos == i + 1)
-            {
-                temp->next = newnode;
-            }
-            else
-            {
-                printf("Invalid position\n");
-            }
+            temp->next = newnode;
+            newnode->next = *list;
         }
+        *list = newnode;
+    } else {
+        NODE *temp = *list;
+        for (int i = 1; i < pos - 1 && temp->next != *list; i++) {
+            temp = temp->next;
+        }
+        newnode->next = temp->next;
+        temp->next = newnode;
     }
 }
 
-void position_delete(NODE **list, int pos)
-{
-    int i = 1;
-    if (*list == NULL)
-    {
+void position_delete(NODE **list, int pos) {
+    if (*list == NULL) {
         printf("List is empty\n");
+        return;
     }
-    else
-    {
-        NODE *temp = *list;
-        if (pos == 1)
-        {
+    NODE *temp = *list;
+    if (pos == 1) {
+        if (temp->next == *list) {
+            free(temp);
+            *list = NULL;
+        } else {
+            NODE *last = *list;
+            while (last->next != *list) {
+                last = last->next;
+            }
             *list = (*list)->next;
+            last->next = *list;
             free(temp);
         }
-        else
-        {
-            NODE *prev = NULL;
-            while (temp->next != NULL && i < pos)
-            {
-                prev = temp;
-                temp = temp->next;
-                i++;
-            }
-            if (i == pos)
-            {
-                prev->next = temp->next;
-                free(temp);
-            }
-            else
-            {
-                printf("Invalid position\n");
-            }
+    } else {
+        NODE *prev = NULL;
+        for (int i = 1; i < pos && temp->next != *list; i++) {
+            prev = temp;
+            temp = temp->next;
+        }
+        if (temp->next == *list) {
+            printf("Invalid position\n");
+        } else {
+            prev->next = temp->next;
+            free(temp);
         }
     }
 }
 
-NODE *orderedins(NODE *list, int x)
-{
-    NODE *newnode, *temp, *prev;
-    newnode = (struct node *)(malloc(sizeof(NODE)));
+NODE *ordered_insert(NODE *list, int x) {
+    NODE *newnode = (NODE *)malloc(sizeof(NODE));
     newnode->info = x;
-    newnode->next = NULL;
-    if (list == NULL)
-        list = newnode;
-    else
-    {
-        temp = list;
-        if (newnode->info < list->info)
-        {
-            newnode->next = list;
-            list = newnode;
-        }
-        else
-        {
-            while (temp->next != NULL && temp->info < newnode->info)
-            {
-                prev = temp;
+    if (list == NULL || x < list->info) {
+        if (list == NULL) {
+            newnode->next = newnode;
+            return newnode;
+        } else {
+            NODE *temp = list;
+            while (temp->next != list) {
                 temp = temp->next;
             }
-            if (temp->info > newnode->info)
-            {
-                newnode->next = temp;
-                prev->next = newnode;
-            }
-            else
-                temp->next = newnode;
+            temp->next = newnode;
+            newnode->next = list;
+            return newnode;
         }
+    } else {
+        NODE *temp = list;
+        while (temp->next != list && temp->next->info < x) {
+            temp = temp->next;
+        }
+        newnode->next = temp->next;
+        temp->next = newnode;
     }
     return list;
 }
