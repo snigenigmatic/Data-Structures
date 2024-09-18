16160bd9e9a457b7ab0cca76f29bb7cca4d3ef0a
snigenigmatic <kaustubhc.11052gmail.com>
2024-09-18 09:22:51 +0530

    Circular Doubly linked lists updated

--- CDLL/cdll.c
+++ CDLL/cdll.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
 
 typedef struct dnode
 {
@@ -8,139 +7,144 @@ typedef struct dnode
     struct dnode *left, *right;
 } DNODE;
 
-DNODE *createNode(int x);
-DNODE *frontins(DNODE *dlist, int x);
-DNODE *endins(DNODE *dlist, int x);
-DNODE *frontdel(DNODE *dlist);
-DNODE *enddel(DNODE *dlist);
-void display(DNODE *dlist)
+DNODE *frontins(DNODE *, int);
+void display(DNODE *);
+DNODE *endins(DNODE *, int);
+DNODE *frontdel(DNODE *);
+DNODE *enddel(DNODE *);
+
+void main()
 {
-    DNODE *temp = dlist;
-    if (dlist == NULL)
-    {
-        printf("The list is empty.\n");
-        return;
-    }
-    else
-    {
-        printf("The list is: ");
-        do
-        {
-            printf("%d ", temp->info);
-            temp = temp->right;
-        } while (temp != dlist);
-        printf("\n");
-    }
-}
+    DNODE *newnode, *dlist;
+    int x, ch, pinfo;
 
-int main()
-{
-    DNODE *dlist = NULL;
-    int choice, x, pinfo;
+    dlist = NULL;
 
-    while (true)
+    while (1)
     {
-        printf("\nDoubly Linked List Operations:\n");
-        printf("1. Front Insertion\n2. Display\n3. End Insertion\n");
-        printf("4. Front Deletion\n5. End Deletion\n6. Exit\n");
-        printf("Enter your choice: ");
-        scanf("%d", &choice);
+        printf(" Enter 1->Front Insertion\n2->Display\n3->end Insertion\n");
+        printf(" Enter 4->Front deletion\n5->End Deletion\n6->exit");
 
-        switch (choice)
+        printf(" Enter YOUR CHOICE");
+        scanf("%d", &ch);
+
+        switch (ch)
         {
         case 1:
-            printf("Enter the information for the node: ");
+        {
+            printf(" Enter the information");
             scanf("%d", &x);
             dlist = frontins(dlist, x);
-            break;
+        }
+        break;
         case 2:
+        {
             display(dlist);
-            break;
+        }
+        break;
         case 3:
-            printf("Enter the information: ");
+        {
+            printf(" Enter the information");
             scanf("%d", &x);
             dlist = endins(dlist, x);
-            break;
+        }
+        break;
         case 4:
+        {
             dlist = frontdel(dlist);
-            break;
+        }
+        break;
         case 5:
+        {
             dlist = enddel(dlist);
-            break;
-        case 6:
-            printf("Exiting the program.\n");
-            exit(0);
+        }
+        break;
+
         default:
-            printf("Invalid choice. Please try again.\n");
+            exit(0);
         }
     }
-
-    return 0;
-}
-
-DNODE *createNode(int x)
-{
-    DNODE *newNode = (DNODE *)malloc(sizeof(DNODE));
-    newNode->info = x;
-    newNode->left = NULL;
-    newNode->right = NULL;
-    return newNode;
 }
 
 DNODE *frontins(DNODE *dlist, int x)
 {
-    DNODE *newNode = createNode(x);
+    DNODE *newnode;
+    newnode = (struct dnode *)(malloc(sizeof(DNODE)));
+    newnode->info = x;
+    newnode->left = NULL;
+    newnode->right = NULL;
+
     if (dlist == NULL)
     {
-        dlist = newNode;
+        dlist = newnode;
+        dlist->right = dlist;
+        dlist->left = dlist;
     }
     else
     {
-        newNode->right = dlist;
-        dlist->left = newNode;
-        newNode->left = dlist->left;
-        dlist->left->right = newNode;
-        dlist->left = newNode;
-        dlist = newNode;
+        newnode->right = dlist;
+        newnode->left = dlist->left;
+        dlist->left = newnode;
+        (newnode->left)->right = newnode;
+        dlist = newnode;
     }
     return dlist;
 }
 
 DNODE *endins(DNODE *dlist, int x)
 {
-    DNODE *newNode = createNode(x);
+    DNODE *newnode;
+    newnode = (struct dnode *)(malloc(sizeof(DNODE)));
+    newnode->info = x;
+    newnode->left = NULL;
+    newnode->right = NULL;
+
     if (dlist == NULL)
     {
-        dlist = newNode;
+        dlist = newnode;
+        dlist->right = dlist;
+        dlist->left = dlist;
     }
     else
     {
-        newNode->right = dlist;
-        newNode->left = dlist->left;
-        dlist->left->right = newNode;
-        dlist->left = newNode;
-        dlist->left->right = newNode;
-        dlist->left = newNode;
+        newnode->right = dlist;
+        newnode->left = dlist->left;
+        dlist->left = newnode;
+        (newnode->left)->right = newnode;
     }
     return dlist;
 }
 
-
-DNODE *frontdel(DNODE *dlist)
+void display(DNODE *dlist)
 {
     DNODE *temp = dlist;
     if (dlist == NULL)
+        printf("Empty List...");
+    else
     {
-        printf("The list is empty.\n");
-        return dlist;
+        while (temp->right != dlist)
+        {
+            printf("\t%d", temp->info);
+            temp = temp->right;
+        }
+        printf("\t%d", temp->info);
     }
+}
+
+DNODE *frontdel(DNODE *dlist)
+{
+    DNODE *temp;
+    if (dlist == NULL)
+        printf("Empty List");
     if (dlist->right == dlist)
     {
+        temp = dlist;
         dlist = NULL;
         free(temp);
     }
     else
     {
+        temp = dlist;
         (dlist->left)->right = dlist->right;
         (dlist->right)->left = dlist->left;
         dlist = dlist->right;
@@ -151,22 +155,20 @@ DNODE *frontdel(DNODE *dlist)
 
 DNODE *enddel(DNODE *dlist)
 {
+    DNODE *temp;
     if (dlist == NULL)
-    {
-        printf("List is empty\n");
-        return dlist;
-    }
-    DNODE *last = dlist->left;
+        printf("Empty List");
     if (dlist->right == dlist)
     {
+        temp = dlist;
         dlist = NULL;
-        free(last);
+        free(temp);
     }
     else
     {
-        last->left->right = dlist;
-        dlist->left = last->left;
-        free(last);
+        temp = dlist->left;
+        dlist->left = temp->left;
+        (dlist->left)->right = dlist;
     }
     return dlist;
 }
